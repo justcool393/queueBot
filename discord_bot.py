56d52432aa52b2b62f38342ed46eae54f0158217
Rick Barber <rick@itsacheckmate.com>
2020-03-01 01:07:31 -0600

    initial

---
+++ discord_bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import tasks
+import os
+from reddit_bot import authorize, get_modqueue_length
+
+client = discord.Client()
+reddit = authorize()
+
+
+@tasks.loop(minutes=30.0)
+async def slow_count():
+    await client.wait_until_ready()
+    for guild in client.guilds:
+        for channel in guild.channels:
+            if channel.name == 'rcoronavirus':
+                print('rc')
+                modqueue_length = await get_modqueue_length(reddit)
+                print('length: {}'.format(modqueue_length))
+                if modqueue_length > 50:
+                    print('modqueue length is {}, sending message'.format(modqueue_length))
+                    await channel.send('Oh no, there are {} items in the modqueue!'.format(modqueue_length))
+
+    print(slow_count.current_loop)
+
+
+slow_count.start()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(os.getenv('DISCORD'))
+
---
+++ reddit_bot.py
@@ -0,0 +1,32 @@
+import praw
+import os
+
+
+def authorize():
+    reddit = praw.Reddit(client_id='rFeHPl-tHNwzeQ',
+                         client_secret=os.environ['REDDITS'],
+                         password=os.environ['REDDITP'],
+                         user_agent='testscript by /u/barber5',
+                         username='barber5')
+    return reddit
+
+def get_modqueue(reddit):
+    print('getting modqueue')
+    return reddit.subreddit('coronavirus').mod.modqueue(limit=None)
+
+async def get_modqueue_length(reddit=None):
+    print('mc time')
+    if reddit is None:
+        print('authing')
+        reddit = authorize()
+
+    modqueue = get_modqueue(reddit)
+    print('got it')
+    length = sum(1 for _ in modqueue)
+    print('length is {}'.format(length))
+    return length
+
+
+
+if __name__ == "__main__":
+    print(get_modqueue_length())
